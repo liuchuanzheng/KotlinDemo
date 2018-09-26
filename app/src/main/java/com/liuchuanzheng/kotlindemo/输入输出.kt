3518b3d0cd5856793b712c9154e48db179593f62
liuchuanzheng <liuchuanzheng@feiliu.com>
2018-09-26 17:36:39 +0800

    输入输出

---
+++ app/src/main/java/com/liuchuanzheng/kotlindemo/输入输出.kt
@@ -0,0 +1,17 @@
+package com.liuchuanzheng.kotlindemo
+
+/**
+ * @author 刘传政
+ * @date 2018/9/26 0026 17:32
+ * QQ:1052374416
+ * 电话:18501231486
+ * 作用:
+ * 注意事项:
+ */
+fun main(args: Array<String>) {
+    var a: Int
+    var b: Int
+    a = readLine()?.toInt() ?: 0
+    b = readLine()?.toInt() ?: 0
+    println(("a+b=" + (a + b)))
+}
