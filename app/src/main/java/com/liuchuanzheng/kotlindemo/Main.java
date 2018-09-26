491e1469d1c004b9dd554a47fbe2fbc9443ab475
liuchuanzheng <liuchuanzheng@feiliu.com>
2018-09-26 15:09:14 +0800

    函数

---
+++ app/src/main/java/com/liuchuanzheng/kotlindemo/Main.java
@@ -0,0 +1,15 @@
+package com.liuchuanzheng.kotlindemo;
+
+/**
+ * @author 刘传政
+ * @date 2018/9/26 0026 15:05
+ * QQ:1052374416
+ * 电话:18501231486
+ * 作用:
+ * 注意事项:
+ */
+public class Main {
+    public static void main(String[] a) {
+        System.out.print("main");
+    }
+}
