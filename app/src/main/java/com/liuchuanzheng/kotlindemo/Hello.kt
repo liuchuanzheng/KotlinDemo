a118d2640b1b81dd9ab714e41af3206ce0eb63a1
liuchuanzheng <liuchuanzheng@feiliu.com>
2018-09-26 11:34:18 +0800

    Initial commit

---
+++ app/src/main/java/com/liuchuanzheng/kotlindemo/Hello.kt
@@ -0,0 +1,10 @@
+package com.liuchuanzheng.kotlindemo
+
+/**
+ * @author 刘传政
+ * @date 2018/9/26 0026 11:14
+ * QQ:1052374416
+ * 电话:18501231486
+ * 作用:
+ * 注意事项:
+ */
---
+++ app/src/main/java/com/liuchuanzheng/kotlindemo/KotlinActivity.kt
@@ -0,0 +1,22 @@
+package com.liuchuanzheng.kotlindemo
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+/**
+ * @author 刘传政
+ * @date 2018/9/26 0026 11:19
+ * QQ:1052374416
+ * 电话:18501231486
+ * 作用:
+ * 注意事项:
+ */
+class KotlinActivity: AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        tv.text = "我来自kotlin"
+
+    }
+}
